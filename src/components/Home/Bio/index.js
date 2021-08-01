f839b7f8c8c6b3de4a423433654199bc1679eee5
JonaPlaz <jplazanet@hotmail.com>
2021-08-01 20:40:51 +0200

    wip integration bien moche :)

--- src/components/Home/Bio/index.js
+++ src/components/Home/Bio/index.js
@@ -6,10 +6,10 @@ import "./bio.scss";
 
 // == Composant Header
 const Bio = () => (
-  <section className="sectionBio">
+  <section className="bio">
     <h2>Bio</h2>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe perspiciatis, voluptate iusto facere inventore tenetur in vitae dolore itaque dicta, sapiente eos ab mollitia laudantium magni ducimus laborum optio omnis. Repellendus et vitae officiis nesciunt odio possimus accusamus sed nihil, soluta sapiente ducimus, animi ad ipsum hic dicta nam quo?
+    <p className="bio-text">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe perspiciatis, voluptate iusto facere inventore tenetur in vitae dolore itaque dicta, sapiente eos ab mollitia laudantium magni ducimus laborum optio omnis. Repellendus et vitae officiis nesciunt odio possimus accusamus sed nihil, soluta sapiente ducimus, animi ad ipsum hic dicta nam quo? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam iure dicta nam magni at asperiores enim nesciunt inventore ipsum quod, nostrum aperiam quisquam nemo repudiandae ab, voluptatum voluptatem tempora quae dolor! Ipsam quaerat perferendis illum? Nostrum ut tempora excepturi atque et error laudantium modi nisi animi adipisci? Aut, beatae ad!
     </p>
   </section>
 );
--- src/components/Home/Project/index.js
+++ src/components/Home/Project/index.js
@@ -6,8 +6,27 @@ import "./project.scss";
 
 // == Composant Header
 const Project = () => (
-  <section className="sectionProject">
-    <p>Projet de la semaine</p>
+  <section className="project">
+    <div className="project-title">
+      <h2>Projet de la Semaine</h2>
+      <h3>Lorem ipsum</h3>
+    </div>
+    <div className="project-layout">
+      <p className="project-description">
+        Descritption. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Voluptates nesciunt quidem consequuntur dolorem eum? Minus commodi harum
+        perferendis sit, architecto labore nisi qui molestias temporibus vitae
+        accusantium numquam cupiditate, dolor perspiciatis enim sequi a repellat
+        eos? Maiores architecto voluptas aliquam impedit aut earum asperiores
+        doloribus, voluptate excepturi tempore, atque in? Lorem ipsum dolor sit,
+        amet consectetur adipisicing elit. Laborum aliquid culpa quisquam non
+        quis. Fuga amet, debitis nemo blanditiis nam nulla impedit consequatur
+        nisi aperiam cupiditate ab ipsa quam aut non explicabo. Neque labore
+        fuga reiciendis perspiciatis aspernatur, doloremque nulla eveniet ipsam
+        voluptatum? Esse adipisci sapiente cum. Et, magnam ex.
+      </p>
+      <div className="project-capture"></div>
+    </div>
   </section>
 );
 
