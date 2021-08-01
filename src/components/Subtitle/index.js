69adbf23386845e43aa68478f418160f36efd6b1
JonaPlaz <jplazanet@hotmail.com>
2021-08-01 18:28:59 +0200

    tentative ajout photo - att meilleur contenu

--- src/components/Subtitle/index.js
+++ src/components/Subtitle/index.js
@@ -4,9 +4,15 @@ import React from "react";
 // == import sass associé
 import "./subtitle.scss";
 
+// == import photo d'identité
+import jonaPic from '../../assets/pics/Jona_photo_v4.jpg';
+
 // == Composant Header
 const Subtitle = () => (
-  <h1 className="subtitle">Développeur Web - REACT.Js - PHP Symfony</h1>
+  <div className="subtitle">
+    <h1 className="subtitle-title">Développeur Web - REACT.Js - PHP Symfony</h1>
+    {/* <img className="subtitle-pic" src={jonaPic} alt="Jonathan Plazanet pic id" /> */}
+  </div>
 );
 
 // == export par défaut
